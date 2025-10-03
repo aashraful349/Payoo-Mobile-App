3ee74a6e18ca58d29849b3166dd89930e3c307ff
aashraful349 <aashraful349@gmail.com>
2025-10-03 22:30:14 +0600

    worked on header of the home page

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-console.log("Hello from script.js");
+// login button functionality
 
-for (let i = 1; i <= 5; i++) {
-    console.log(i);
-}
+document.getElementById('loginButton').addEventListener('click', function(e) {
+    e.preventDefault();
+    const mobileNumber = 12345;
+    const pinNumber = 1234;
+    const mobileNumberValue=document.getElementById('mobile-number').value;
+    const mobileNumberValueConverted= parseInt(mobileNumberValue);
+    const pinNumberValue=document.getElementById('pin-number').value;
+    const pinNumberValueConverted=parseInt(pinNumberValue);
+    if(mobileNumber===mobileNumberValueConverted && pinNumber===pinNumberValueConverted){
+        window.location.href='./home.html';
+    }
+    else{
+        alert('Login Failed');
+    }
+});
