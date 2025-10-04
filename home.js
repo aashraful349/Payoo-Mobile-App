5db2e9cb7c48b9e5c80131a3c70f6099ce9d6824
aashraful349 <aashraful349@gmail.com>
2025-10-04 09:54:17 +0600

    home screen almost complete.add the add amount feature and it is working as expected

--- home.js
+++ home.js
@@ -1,1 +1,13 @@
-console.log("This is home page");       
+document.getElementById('ad-money-btn').addEventListener('click',function(e){
+    e.preventDefault();
+    const bank=document.getElementById('bank').value;
+    const accountNumber=document.getElementById('account-number').value;
+    const addAmount=parseInt(document.getElementById('add-amount').value);
+    const addPin=document.getElementById('add-pin').value;
+    // console.log(bank,accountNumber,addAmount,addPin);
+    const availableBalance=parseInt(document.getElementById('available-balance').innerText);
+    // console.log(availableBalance);
+    const totalNewAvailableBalance=availableBalance+addAmount;
+    // console.log(totalNewAvailableBalance);
+    document.getElementById('available-balance').innerText=totalNewAvailableBalance;
+})
