3238b90bd8f45ffd28a23f98c47213016ebb4b55
aashraful349 <aashraful349@gmail.com>
2025-10-05 07:28:42 +0600

    made some minor changes

--- script.js
+++ script.js
@@ -2,12 +2,16 @@
 
 document.getElementById('loginButton').addEventListener('click', function(e) {
     e.preventDefault();
-    const mobileNumber = 12345;
+    const mobileNumber = 12345678910;
     const pinNumber = 1234;
     const mobileNumberValue=document.getElementById('mobile-number').value;
     const mobileNumberValueConverted= parseInt(mobileNumberValue);
     const pinNumberValue=document.getElementById('pin-number').value;
     const pinNumberValueConverted=parseInt(pinNumberValue);
+    if(mobileNumberValue.length!=11){
+        alert('Mobile number must be 11 digit');
+        return;
+    }
     if(mobileNumber===mobileNumberValueConverted && pinNumber===pinNumberValueConverted){
         window.location.href='./home.html';
     }
